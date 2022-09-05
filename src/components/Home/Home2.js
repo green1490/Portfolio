33a3eecc07bc588fabc328494f9f7b5f5a16081a
green1490 <zambokrisztian1490@gmail.com>
2022-09-05 17:22:42 +0200

    change linkedin link

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -64,7 +64,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/krisztián-zámbó-2399631b5"
+                  href="https://www.linkedin.com/in/krisztián-zámbó"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
